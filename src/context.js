413ce072c7018a55dce2bf0b01ab356048d4f4a9
valentinaloreto <vloretocastillo@gmail.com>
2020-01-26 19:45:52 +0100

    implemented fetch and added .env file at root level

--- src/context.js
+++ src/context.js
@@ -4,11 +4,22 @@ const Context = React.createContext()
 export class Provider extends React.Component {
 
     state = {
-        track_list : [
-            {track : { trackname: 'abc'} },
-            {track : { trackname: 'abc'} },
-        ], 
+        track_list : [], 
         heading : 'Top 10 tracks'
+    }
+
+    componentDidMount() {
+        // console.log(process.env.REACT_APP_MM_KEY)
+        fetch(`https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=nl&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY}`, {
+            method: 'GET',
+            // contentType: 'application/json',
+            // mode: 'no-cors'
+        })
+        // .then( res => res.json() )
+        .then( data => {
+            console.log(data)
+        })
+        .catch(err => console.log(err))
     }
 
 
