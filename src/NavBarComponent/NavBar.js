c5c5fa40d77e1931071748fc0f4574decf8d8064
valentinaloreto <vloretocastillo@gmail.com>
2020-01-27 13:04:57 +0100

    added icons and css styles to tracks and track UIs

--- src/NavBarComponent/NavBar.js
+++ src/NavBarComponent/NavBar.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import './NavBar.css'
 
 const Navbar = () => {
 
     return (
           <nav>
-            <div className="nav-wrapper teal accent-4">
-                
+            <div className="nav-wrapper teal accent-4 navbarcomponent">
+                <h5>LyricFinder</h5>
             </div>
         </nav>
     )
