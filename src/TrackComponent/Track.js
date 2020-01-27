6ba19cc4a509470a09ec66fbca4ef611cfd579e1
valentinaloreto <vloretocastillo@gmail.com>
2020-01-27 11:04:41 +0100

    added icons and css styles to tracks and track UIs

--- src/TrackComponent/Track.js
+++ src/TrackComponent/Track.js
@@ -1,14 +1,17 @@
 
 import React from 'react'
+import './Track.css';
 
 
 const Track = (props) => {
 
     return (
-        <div className='card-panel teal'>
-            Title: { props.title }
-            Artist: { props.artist }
-            Album: { props.album }
+        <div className='card-panel teal accent-4 trackcomponent-item'>
+            <h6>{ props.artist } </h6>
+            
+            <span className='icons'><ion-icon name="play-circle" className="icon"></ion-icon></span> Track: { props.title }
+            <br/>
+            <span className='icons'><ion-icon name="albums" className="icon"></ion-icon></span> Album: { props.album }
         </div>
     )
 
--- src/TracksComponent/Tracks.js
+++ src/TracksComponent/Tracks.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Consumer } from '../context';
 import Loader from '../LoaderComponent/Loader';
 import Track from '../TrackComponent/Track';
+import './Tracks.css';
+
 
 
 
@@ -12,7 +14,6 @@ class Tracks extends React.Component {
         return (
             <Consumer>
             { (value) => {
-                {/* console.log(value) */}
                 let { track_list } = value
                 let UIComponent
                 if (track_list.length === 0) {
@@ -20,10 +21,10 @@ class Tracks extends React.Component {
                 } else {
                     UIComponent = track_list.map(el => {
                         let { track } = el
-                        let {track_name, track_id, album_name, artist_name, primary_genres } = track
+                        let {track_name, track_id, album_name, artist_name, primary_genres } = el.track
                         let { music_genre_list } = primary_genres
                         let genres = music_genre_list.map(el => el.music_genre.music_genre_name )
-                        {/* console.log(track, genres) */}
+                        console.log(track, genres)
                         return <Track key={track_id} title={ track_name } album={ album_name } artist={ artist_name } genres={ genres }/>
                     })
                     {/* UIComponent = <div  >hi</div> */}
@@ -31,7 +32,9 @@ class Tracks extends React.Component {
                 
                 
                 return (
-                    UIComponent
+                    <div className='trackscomponent-container'>
+                        {UIComponent}
+                    </div>
                 )
             } }
             </Consumer>
