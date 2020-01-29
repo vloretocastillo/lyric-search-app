745ee4eb2c3e4ae2c82160511ffa477e660706b9
valentinaloreto <vloretocastillo@gmail.com>
2020-01-29 14:44:11 +0100

    added icons and css styles to tracks and track UIs

--- src/LyricsComponent/Lyrics.js
+++ src/LyricsComponent/Lyrics.js
@@ -2,6 +2,40 @@ import React from 'react';
 // import './App.css';
 
 class Lyrics extends React.Component {
+    state = {
+        track: {},
+        lyrics: {}
+
+    }
+
+    getTrackInfo = () => {
+        let url = `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.get?track_id=${ this.props.match.params.id }&apikey=${process.env.REACT_APP_MM_KEY}`
+        fetch(url)
+        .then( res => res.json() )
+        .then( data => {
+            this.setState({ track : data.message.body.track })
+        })
+        .catch(err => console.log(err))
+    }
+
+    getLyricsInfo = () => {
+        let url = `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${ this.props.match.params.id }&apikey=${process.env.REACT_APP_MM_KEY}`
+        fetch(url)
+        .then( res => res.json() )
+        .then( data => {
+            this.setState({ lyrics : data.message.body.lyrics })
+        })
+        .catch(err => console.log(err))
+    }
+
+    componentDidMount() {
+        this.getLyricsInfo()
+        this.getTrackInfo()
+
+        console.log(this.state.lyrics)
+        console.log(this.state.track)
+
+    }
     render () {
         return (
             <h1>
