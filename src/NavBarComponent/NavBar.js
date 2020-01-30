7ae7c6f3882a3be813c32b6a88344386371044a3
valentinaloreto <vloretocastillo@gmail.com>
2020-01-30 19:18:09 +0100

    Added Search UI  component

--- src/NavBarComponent/NavBar.js
+++ src/NavBarComponent/NavBar.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './NavBar.css'
+import { Link } from 'react-router-dom'
+
 
 const Navbar = () => {
 
     return (
           <nav>
             <div className="nav-wrapper teal accent-4 navbarcomponent">
-                <h5>LyricFinder</h5>
+            <Link to='/'> <h5>LyricFinder </h5></Link>
             </div>
         </nav>
     )
