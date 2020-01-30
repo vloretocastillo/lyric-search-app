f19b4c8ddb286c93b8438c333d80194d91917445
valentinaloreto <vloretocastillo@gmail.com>
2020-01-30 19:38:44 +0100

    implementing the search track logic

--- src/SearchComponent/Search.js
+++ src/SearchComponent/Search.js
@@ -11,13 +11,38 @@ class Search extends React.Component {
         title : ''
     }
 
+    handleChange(e) {
+        this.setState({ title: e.target.value})
+    }
+
+    handleSumbmit(e) {
+        e.preventDefault();
+        let url = `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search?q_track=${ this.state.title }&apikey=${process.env.REACT_APP_MM_KEY}`
+        fetch(url)
+            .then( res => res.json() )
+            .then( data => console.log(data))
+            .catch(err => console.log(err))
+    }
+
+    
     render () {
         return (
             <Consumer>
             { (value) => {
                 return (
                     <div className='input-wrapper'>
-                        <input type="text" id="input" className="input-track-title" placeholder="Search By Track Title" />
+                        <form action="" onSubmit={ (e)=> this.handleSumbmit(e) }>
+                        <input 
+                            type="text" 
+                            id="input" 
+                            className="input-track-title" 
+                            placeholder="Search By Track Title" 
+                            name='title' 
+                            value={this.state.title}
+                            onChange={ (e) => this.handleChange(e) }
+                            autoComplete="off"
+                        />
+                        </form>
                     </div>
                 )
             }}
