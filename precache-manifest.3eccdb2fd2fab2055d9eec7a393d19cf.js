f293c331d87c862d26c72ca6996090c8e30af18a
valentinaloreto <vloretocastillo@gmail.com>
2020-02-02 15:43:19 +0100

    Updates

--- precache-manifest.3eccdb2fd2fab2055d9eec7a393d19cf.js
+++ precache-manifest.3eccdb2fd2fab2055d9eec7a393d19cf.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5934745305f8bcd84a35fa4c0025a54f",
+    "revision": "1abab1bd75c2baf399dfec109a277d87",
     "url": "/lyric-search-app/index.html"
   },
   {
-    "revision": "8a3e844c238778697651",
+    "revision": "a8c7095d53693ed93932",
     "url": "/lyric-search-app/static/css/main.2c3221bd.chunk.css"
   },
   {
-    "revision": "bd73d2fe80bd9f738e22",
-    "url": "/lyric-search-app/static/js/2.f478b65b.chunk.js"
+    "revision": "a0a13d1967379b462f98",
+    "url": "/lyric-search-app/static/js/2.333f53e6.chunk.js"
   },
   {
     "revision": "e928fe768baa9832b5bc57eae021f30c",
-    "url": "/lyric-search-app/static/js/2.f478b65b.chunk.js.LICENSE"
+    "url": "/lyric-search-app/static/js/2.333f53e6.chunk.js.LICENSE"
   },
   {
-    "revision": "8a3e844c238778697651",
-    "url": "/lyric-search-app/static/js/main.708b8815.chunk.js"
+    "revision": "a8c7095d53693ed93932",
+    "url": "/lyric-search-app/static/js/main.383fe263.chunk.js"
   },
   {
     "revision": "27ceb5087a99a72ad7c2",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/lyric-search-app/precache-manifest.a2eca74fe54f168895ae0c83bce8e2d3.js"
+  "/lyric-search-app/precache-manifest.3eccdb2fd2fab2055d9eec7a393d19cf.js"
 );
 
 self.addEventListener('message', (event) => {
